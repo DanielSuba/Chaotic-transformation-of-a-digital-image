fc4c1b9dd0bdf180f0f930b806018ecfd0348a73
DanielSuba <danielius2005m@gmail.com>
2026-04-11 15:53:32 +0300

    Add libralies for interface

---
+++ interface.py
@@ -0,0 +1,5 @@
+import sys
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout,
+                             QHBoxLayout, QPushButton, QComboBox, QLineEdit,
+                             QLabel, QFileDialog, QGroupBox, QMessageBox)
+from PyQt6.QtCore import Qt
